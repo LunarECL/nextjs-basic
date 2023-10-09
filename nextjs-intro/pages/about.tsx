04f00296605711e44d0541af791772440cf6cba1
LunarECL <jkjk98765@naver.com>
2023-10-09 14:28:51 -0400

    feat: nextjs-intro initialize with basic router

---
+++ nextjs-intro/pages/about.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/pages/components/NavBar";
+
+export default function About() {
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
+}
---
+++ nextjs-intro/pages/index.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/pages/components/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Home</h1>
+      <p>My Home page</p>
+    </div>
+  );
+}
